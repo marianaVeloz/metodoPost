18112a84b55aab691fb8da21ae2be4478e6d812e
Sergio-bot-16 <sergioeloycruz@gmail.com>
2026-03-08 19:43:38 -0600

    "Arreglo de errores, e integramiento de diseño"

--- producto/src/app/app.config.ts
+++ producto/src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http'; // 1. Importar esto
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes),
     provideHttpClient()
   ]
 };
